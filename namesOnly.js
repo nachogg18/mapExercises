56092a69f3255d4d1c881f96f247dbb1839b4247
nachogg18 <ignacio.galvez@alumnos.frm.utn.edu.ar>
2020-07-18 19:54:03 -0300

    agregado namesOnly.js

---
+++ namesOnly.js
@@ -0,0 +1,28 @@
+function namesOnly(arr){
+    return arr.map((el) => el["name"])
+  }
+  
+  console.log(namesOnly([
+      {
+          name: "Angelina Jolie",
+          age: 80
+      },
+      {
+          name: "Eric Jones",
+          age: 2
+      },
+      {
+          name: "Paris Hilton",
+          age: 5
+      },
+      {
+          name: "Kayne West",
+          age: 16
+      },
+      {
+          name: "Bob Ziroll",
+          age: 100
+      }
+  ])); 
+  // ["Angelina Jolie", "Eric Jones", "Paris Hilton", "Kayne West", "Bob Ziroll"]
+  
